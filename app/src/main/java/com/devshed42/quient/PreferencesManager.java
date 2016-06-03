e801340c9136ec46531016a2735a1a5007e3d430
Doug Reimer <nicad49@gmail.com>
2016-06-02 23:14:47 -0600

    Adding Preferences Manager; not currently implemented

---
+++ app/src/main/java/com/devshed42/quient/PreferencesManager.java
@@ -0,0 +1,63 @@
+package com.devshed42.quient;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by doug on 2016-06-02.
+ */
+public class PreferencesManager {
+
+    private static final String PREFS_NAME = "QuientPreferences";
+
+    public static final String PREF_STATUS = "STATUS";
+    public static final boolean PREF_STATUS_DEFAULT = false;
+
+    public static final String PREF_ACTION = "ACTION";
+
+    public static final String PREF_ORIGINAL_STATE = "ORIGINAL_STATE";
+    public static final int PREF_ORIGINAL_STATE_DEFAULT = 2;
+
+    public static final int VIBRATE = 1;
+    public static final int SILENT = 0;
+    public static final int PREF_SAVED_MODE_NO_VALUE = 2;
+    
+
+    public static final void saveState(Context context, boolean enabled) {
+        context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+                .edit()
+                .putBoolean(PREF_STATUS, enabled)
+                .commit();
+    }
+
+    public static final boolean getSavedState(Context context) {
+        return context.getSharedPreferences(PREFS_NAME,Context.MODE_PRIVATE)
+                .getBoolean(PREF_STATUS,PREF_STATUS_DEFAULT);
+    }
+
+    public static final void saveAction(Context context, int action) {
+        context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+                .edit()
+                .putInt(PREF_ACTION, action)
+                .commit();
+    }
+
+    public static final int getSavedAction(Context context) {
+        return context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+                .getInt(PREF_ACTION,VIBRATE);
+    }
+
+    public static final void saveOriginalState(Context context, int ringMode) {
+        context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+                .edit()
+                .putInt(PREF_ORIGINAL_STATE, ringMode)
+                .commit();
+    }
+
+    public static final int getOriginalState(Context context) {
+        return context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+                .getInt(PREF_ORIGINAL_STATE, PREF_ORIGINAL_STATE_DEFAULT);
+    }
+
+}
